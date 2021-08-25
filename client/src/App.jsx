b1d7c579bf6d40a9b15a3b698f949b458c35c4de
Aadhinana <ajibaa@gmail.com>
2021-08-26 01:15:09 +0530

    started off with forntend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,39 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [voteCount1, setVoteCount1] = useState(0);
+  const [voteCount2, setVoteCount2] = useState(0);
+
+  const [walletBalance, setWalletBalance] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  const handleVote = (votedFor) => {
+    console.log("voted for", votedFor)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      <h1>Voting Contracts</h1>
+      <p>Connect your wallet</p>
+      { !address &&<button>Connect Wallet</button> }
+      {address && <p>{address} is the user address that is connected right now!</p>}
+      {walletBalance && <p>{walletBalance} is the amount of money this connected user has</p>}
+      <br />
+
+      <div>
+        <h2>Candidate #1 Details</h2>
+        <p>{voteCount1} votes</p>
+        <button onClick={handleVote.bind(this, 1)}>Vote for 1</button>
+      </div>
+
+      <div>
+        <h2>Candidate #2 Details</h2>
+        <p>{voteCount2} votes</p>
+        <button onClick={handleVote.bind(this, 2)} > Vote for 2</button>
+      </div>
+    </div >
   )
 }
 
